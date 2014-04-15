751b44c5ed2bc5eb855d12e422a53e22b4fe8342
Benno van Assen <vanassen.benno@gmail.com>
2014-04-15 16:23:56 +0200

    Voortgang Kalishoek

--- php/header.php
+++ php/header.php
@@ -29,7 +29,9 @@
     <!-- Head scripts, avoid including scripts here -->
     <!-- Don't forget to compile and minify the used modernizr features, or to remove it when unused -->
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/standalone/modernizr.js"></script>
-
+    
+    <script type="text/javascript" src="//use.typekit.net/nva7lcq.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
     <!-- Wordpress head function -->
     <?php wp_head(); ?>
 
@@ -37,83 +39,53 @@
   <body <?php body_class(); ?> >
 
   <!-- Include svg icons -->
-  <?php include_once("ico/sprites.svg"); ?>
+  
 
+  <!-- Header -->  
   <header class="Header">
-    <div class="Header-contact">
-      <div class="u-gridContainer">
-        <a href="#" class="Button Button--default">
-          <svg class="Icon" viewBox="0 0 128 128"><use xlink:href="#icon-phone"></use></svg>
-          <span>076 - 5962069</span>
-        </a>
-        <a href="#" class="Button Button--default">
-          <svg class="Icon" viewBox="0 0 128 128"><use xlink:href="#icon-envelope"></use></svg>
-          <span>info@jacobssierbestrating.nl</span>
-        </a>
-        <span class="u-objRight">
-          <a href="#" class="Button Button--brand">
-            <span>Ga naar webshop</span>
-            <svg class="Icon" viewBox="0 0 128 128"><use xlink:href="#icon-arrow-right2"></use></svg>
-          </a>
-        </span>
+    
+    <div class="Header-logo">
+        <div class="u-gridContainer Informatie">
+        
+          <div class="Logo">
+              <a href="/" class="Logo-imageLink"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/kalishoek-logo.svg" alt="Logo"></a>
+              <a href="/" class="Logo-textLink"><h1 class="Logo-heading"><span class="Logo-headingTop">Kalishoek Slotenservice</span>
+                <span class="Logo-headingBottom">De slotenmaker voor Rotterdam en regio Hoekse Waard & Drechtsteden</span></h1></a>
+          </div>
+          <span class="u-objRight Phone">
+          
+            Bel ons voor meer informatie
+        <span class="Phone-nr">0180-394444</span></h1></a>
+
+          
+          </span>
       </div>
     </div>
-    <div class="Header-logo">
-        <div class="u-gridContainer">
-          <div class="Logo">
-              <a href="/" class="Logo-imageLink"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-jacobs.svg" alt="Jacobs Sierbestrating Logo"></a>
-              <a href="/" class="Logo-textLink"><h1 class="Logo-heading"><span class="Logo-headingTop">Jacobs</span><span class="Logo-headingBottom">Sierbestrating</span></h1></a>
-          </div>
-          <span class="u-objRight">
-            <table class="Table Table--invertedBold">
-                <thead>
-                <tr>
-                    <td class="Table-title">Openingstijden</td>
-                </tr>                    
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Ma t/m vr</td>
-                    <td>7:00 t/m 17:00</td>
-                </tr>
-                <tr>
-                    <td>Zaterdag</td>
-                    <td>8:30 t/m 12:30</td>
-                </tr>
-                <tr>
-                    <td>Zondag</td>
-                    <td>Gesloten</td>
-                </tr>
-                </tbody>
-            </table>
-          </span>
-        </div>
-    </div>
+
     <div class="Header-nav">
         <div class="u-gridContainer">
             <!-- Main navigation -->
             <?php include 'includes/navigation.php'; ?>
+
         </div>
     </div>
-    <?php if(is_front_page()) {?><!-- Display banner only on the frontpage -->
-    <div class="Header-carousel">
-        <div class="u-gridContainer">
-            <div class="Offer">
-                <div class="Offer-image"></div>
-                <div class="Offer-text">
-                    <h2 class="Offer-title">Garden Concept Line</h2>
-                    <p class="Offer-subtitle">20x30cm</p>
-                    <p class="offer-content">Dit is een eerste keus tegel met de befaamde coating. Gemakkelijk in onderhoud, geen alg aangroei, bijna geen witte uitslag.</p>
-                    <div class="price-section">
-                        <p class="offer-oldprice">Van € 19,00</p>
-                        <h3 class="offer-currentprice">Nu € 14,50</h3>
-                        <a href="#" class="Button Button--brand">Bekijk aanbieding</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+  
+
+ <!-- Slider -->
+  <div class="u-gridRow">
+      <div class="Slider-frame">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/kalishoek3.jpg" />
+      </div>
     </div>
-    <?php }?>
+  </div>
+
+    
+    <div class="u-gridRow bgwhite">
+      <div class="u-gridContainer">
+          <?php if(is_front_page()) {?><!-- Display banner only on the frontpage -->
+          <?php }?>
+      </div>  
+    </div>
   </header>
 
 
---
+++ php/page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+	<div>
+		<div class="u-gridContainer">
+		
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+			<article class="col7" id="post-<?php the_ID(); ?>">
+				<?php if(!is_front_page()) { ?>
+				<h2><?php the_title(); ?></h2>
+				<?php } ?>
+				<div>
+					<?php the_content(); ?>
+					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+				</div>
+			</article>
+		
+		<?php endwhile; endif; ?>
+
+			<div class="Sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
